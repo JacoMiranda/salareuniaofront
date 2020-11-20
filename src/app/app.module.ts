076a908c71a6cc6c54a9857268a27a21411c39c0
unknown <jaco-miranda@hotmail.com>
2020-11-20 08:51:23 -0400

    Criação do serviço e componentes da sala

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { ListRoomComponent } from './list-room/list-room.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomDetailsComponent,
+    CreateRoomComponent,
+    ListRoomComponent,
+    UpdateRoomComponent
   ],
   imports: [
     BrowserModule,
