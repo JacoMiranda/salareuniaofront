3f20a100021451e4dfc1550ca2365f84a60c6a85
unknown <jaco-miranda@hotmail.com>
2020-11-20 19:23:54 -0400

    criando html para atualização dos dados  da sala

--- src/app/list-room/list-room.component.ts
+++ src/app/list-room/list-room.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
-@Component({
+import { RoomDetailsComponent} from 'src/app/room-details/room-details.component';
+import { from, Observable} from "rxjs";
+import { RoomService} from  "../room.service";
+import {Room} from "../room";
+import {Router} from '@angular/router'
+Component({
   selector: 'app-list-room',
   templateUrl: './list-room.component.html',
   styleUrls: ['./list-room.component.css']
 })
 export class ListRoomComponent implements OnInit {
+rooms: Observable<Room[]>;
 
-  constructor() { }
+  constructor(private RoomService: RoomService,
+    private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.reloadData();
   }
+  reloadData() {
+   this.rooms = this.RoomService.getRoomList();
+  }
+
+  deleteRoom(id: number){
+    this.RoomService.deleteRoom(id).subscribe(
+      data => {
+        console.log(data)
+        this.reloadData();
+      },
+      error => console.log(error)
+    );
+  }
+  roomDetails(id: number){
+    this.router.navigate(['details', id]);
+  }
+  updateRoom(id: number){
+    this.router.navigate(['upadate', id])
+  }
+
 
 }
